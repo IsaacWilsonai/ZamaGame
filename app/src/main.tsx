66ffac4fd193c3fb80472de95aeb4bad7b1e1eb8
CassiusDe <ouzacpj605@hotmail.com>
2025-09-25 12:52:44 +0800

    deploy to netlify

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,32 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-
-// Wagmi and RainbowKit imports
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import '@rainbow-me/rainbowkit/styles.css';
-import { WagmiProvider } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-
-const config = getDefaultConfig({
-  appName: 'ZamaGame',
-  projectId: 'YOUR_PROJECT_ID', // Get from https://cloud.walletconnect.com
-  chains: [sepolia],
-  ssr: false,
-});
-
-const queryClient = new QueryClient();
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
